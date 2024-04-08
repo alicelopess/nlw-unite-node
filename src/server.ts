90d5e4e4934ab088213c9b67a5b74fc7a004dd54
Alice Lopes <alicedflopes.02@gmail.com>
2024-04-08 20:21:50 -0300

    feat(api): add event creation route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,41 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
 
+//DB Conection
+const prisma = new PrismaClient({
+    log: ['query'],
+})
+
 app.get('/', () => {
     return 'Hello, NLW Unite!'
+})
+
+//Create event
+app.post('/events', async (request, reply) => {
+    
+    const createEventSchema = z.object({
+        title: z.string().min(4, { message: "Must be 5 or more characters long" }),
+        details: z.string().max(100, { message: "Must be 100 or fewer characters long" }).nullish(),
+        maximunAttendees: z.number().int().positive().nullish(),
+    })
+
+    //Validation
+    const eventData = createEventSchema.parse(request.body)
+
+    //Implement on DB
+    const createdEvent = await prisma.event.create({
+        data: {
+            title: eventData.title,
+            details: eventData.details,
+            maximunAttendees: eventData.maximunAttendees,
+            slug: new Date().toISOString(),
+        },
+    })
+
+    return reply.status(201).send({ eventId: createdEvent.id })
 })
 
 app.listen({ port: 3333 })
